75d4b03e3f23a56b4c159cabc11ecf0f9aefb1e7
Anttoni Tukia <46928496+anttonisamuel@users.noreply.github.com>
2019-06-13 11:32:56 +0300

    Added function to show questions and choices

--- quiz.js
+++ quiz.js
@@ -1,4 +1,3 @@
-
 // Select all elements
 const start = document.getElementById("start");
 const quiz = document.getElementById("quiz");
@@ -44,4 +43,30 @@ let questions = [
     choiceC : "39",
     choiceD : "27"
   }
-]
+];
+const lastQuestion = questions.lenght -1;
+let currentQuestion = 0;
+
+//Function to render each question
+function showQuestion(){
+  let q = questions[currentQuestion];
+  question.innerHTML = "<p>" + q.question + "</p>";
+  //INSERT IMAGE
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choicD.innerHTML = q.choiceD;
+};
+
+//Hide the star button and start the quiz
+start.style.display = "none";
+showQuestions();
+quiz.style.display = "block";
+
+//Funtion to render progress
+funtion showProgress(){
+  for (let i = 0; i <= lastQuestion; i++)
+  {
+    //progress.innerHTML += "";
+  }
+}
